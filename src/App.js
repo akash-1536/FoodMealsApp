915d0b1e49e5a3cbaacaa0e944aba4b42976a3c0
Akash A <60578239+akash-1536@users.noreply.github.com>
2023-10-16 04:23:16 +0000

    Cart and Modal UI added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import { Fragment, useState } from "react";
 import React from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 const App = () => {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
-    <div>
-      <Header />
+    <Fragment>
+      {cartIsShown && <Cart onClose = {hideCartHandler}/>}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </div>
+    </Fragment>
   );
 };
 
